2fdf50a5024e284e5b5ce23e87da0cf79c34c310
Sayyed Sania <sayyedsania87@example.com>
2026-06-11 22:07:25 +0530

    fix supabase env issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,33 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Topbar from "@/components/Topbar";
 import MobileNav from "@/components/MobileNav";
 import { supabase } from "@/lib/supabase";
 import Sidebar from "@/components/Sidebar";
 import BentoGrid from "@/components/BentoGrid";
 
-export default async function Home() {
-  const { data, error } = await supabase
-    .from("courses")
-    .select("*");
+export default function Home() {
+  const [courses, setCourses] = useState([]);
 
-  if (error) {
-    return (
-        
-      <main className="min-h-screen bg-black text-white flex items-center justify-center">
-        Failed to load courses
-      </main>
-    );
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase
+        .from("courses")
+        .select("*");
+
+      if (!error) setCourses(data);
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    
     <main className="min-h-screen bg-[#050505] text-white flex">
       <Sidebar />
-
       <section className="flex-1 p-6 pb-24 md:pb-6">
-        <Topbar/>
-        <BentoGrid courses={data} />
+        <Topbar />
+        <BentoGrid courses={courses} />
       </section>
       <MobileNav />
     </main>
